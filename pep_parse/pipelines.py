bcc16d894e3dec186626012c57bc87ec45716c9d
Mitsushidu <fireminergoodplay@gmail.com>
2024-01-23 15:24:45 +0300

    pep fixes, refactoring:

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,23 +1,23 @@
 import csv
 import datetime as dt
+from collections import defaultdict
+
 from pep_parse.settings import BASE_DIR
 
 
 class PepParsePipeline:
     def open_spider(self, spider):
         self.filepath = BASE_DIR / 'results'
-        self.status_count = {}
+        self.status_count = defaultdict(int)
 
     def process_item(self, item, spider):
-        if item['status'] not in self.status_count.keys():
-            self.status_count[item['status']] = 1
-        else:
-            self.status_count[item['status']] += 1
+        self.status_count[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        current_datetime = dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        current_datetime = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         filename = f'status_summary_{current_datetime}.csv'
+        self.status_count['Total'] = sum(self.status_count.values())
         with open(
             self.filepath / filename,
             mode='w',
@@ -30,6 +30,4 @@ class PepParsePipeline:
                 quotechar='|'
             )
             writer.writerow(['Статус', 'Количество'])
-            for key in self.status_count.keys():
-                writer.writerow([key, self.status_count[key]])
-            writer.writerow(['Total', sum(self.status_count.values())])
+            writer.writerows(self.status_count.items())
